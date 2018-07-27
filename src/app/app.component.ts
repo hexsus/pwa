b2758dfe70294a4a61741bb32ef3a6d42727fa99
Nikita <heksus23@gmail.com>
2018-07-27 18:28:43 +0300

    add btn for add app to screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,25 +7,33 @@ import Siema from 'siema';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'pwa';
-
   ngOnInit() {
-    const siema = new Siema({
-      selector: '.siema',
-      duration: 200,
-      easing: 'ease-out',
-      perPage: 1,
-      startIndex: 0,
-      draggable: true,
-      multipleDrag: true,
-      threshold: 20,
-      loop: false,
-      rtl: false,
-      onInit: () => {},
-      onChange: () => {},
+    new Siema();
+    let deferredPrompt;
+    const btnAdd = document.getElementById('addToHome');
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      btnAdd.style.display = 'block';
     });
 
-    document.querySelector('.prev').addEventListener('click', () => siema.prev());
-    document.querySelector('.next').addEventListener('click', () => siema.next());  
+    btnAdd.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      btnAdd.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+    });
   }
 }
