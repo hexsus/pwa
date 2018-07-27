d5806bfe6cc40cbd9ab409d59f8d9bcdf8003978
Nikita <heksus23@gmail.com>
2018-07-27 14:27:15 +0300

    add slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Siema from 'siema';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa';
+
+  ngOnInit() {
+    const siema = new Siema({
+      selector: '.siema',
+      duration: 200,
+      easing: 'ease-out',
+      perPage: 1,
+      startIndex: 0,
+      draggable: true,
+      multipleDrag: true,
+      threshold: 20,
+      loop: false,
+      rtl: false,
+      onInit: () => {},
+      onChange: () => {},
+    });
+
+    document.querySelector('.prev').addEventListener('click', () => siema.prev());
+    document.querySelector('.next').addEventListener('click', () => siema.next());  
+  }
 }
