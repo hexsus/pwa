ec860006da9994a9108b0cd1cdbf05d400f14114
Nikita <heksus23@gmail.com>
2018-09-03 21:33:20 +0300

    add event.preventDefault for beforeinstallprompt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,25 @@ export class AppComponent implements OnInit {
   }
 
   public initAppToScreen() {
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // e.preventDefault();
-      this.deferredPrompt = e;
+    window.addEventListener('beforeinstallprompt', (event) => {
+      // Prevent Chrome <= 67 from automatically showing the prompt
+      event.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = event;
+      // Update the install UI to notify the user app can be installed
     });
   }
 
   public addToHomeScreen(): void {
     this.deferredPrompt.prompt();
-    this.deferredPrompt.userChoice.then(() => (this.deferredPrompt = null));
+    this.deferredPrompt.userChoice.then((choice) => {
+      if (choice.outcome === 'accepted') {
+        alert('User accepted the A2HS prompt');
+      } else {
+        alert('User dismissed the A2HS prompt');
+      }
+      // Clear the saved prompt since it can't be used again
+      this.deferredPrompt = null;
+    });
   }
 }
