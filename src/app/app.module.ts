e8f3f2784bf6ab54c11760ec5d329e76232b4cbe
Nikita <heksus23@gmail.com>
2018-09-06 17:15:08 +0300

    add auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,17 @@ import { TimerComponent } from './timer/timer.component';
 import { MinuteToSecondsPipe } from './minute-to-seconds.pipe';
 import { HeaderComponent } from './header/header.component';
 import { CoreModule } from './core/core.module';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+const config = {
+  apiKey: 'AIzaSyC-sLDvcyvjR5h4N4qMCTQ-qGHrXbRDjCY',
+  authDomain: 'time-evolve.firebaseapp.com',
+  databaseURL: 'https://time-evolve.firebaseio.com',
+  projectId: 'time-evolve',
+  storageBucket: 'time-evolve.appspot.com',
+  messagingSenderId: '433462159797'
+};
 
 @NgModule({
   declarations: [
@@ -19,6 +30,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     CoreModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,10 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+// import { Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import * as firebase from 'firebase';
+
+interface User {
+  uid: string;
+  email: string;
+  photoURL?: string;
+  displayName?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user: Observable<User>;
 
-  constructor() {
+  constructor(private afAuth: AngularFireAuth,
+              private afs: AngularFirestore) {
+    this.user = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        } else {
+          of(null);
+        }
+      }));
+  }
+
+  googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return this.oAthLogin(provider);
+  }
+
+  private oAthLogin(provider) {
+    return this.afAuth.auth.signInWithPopup(provider).then(credentials => {
+      this.updateUserData(credentials.user);
+    });
+  }
+
+  updateUserData(user) {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+    const data: User = {
+      uid: user.uid,
+      email: user.email,
+      photoURL: user.photoURL,
+      displayName: user.displayName
+    };
+
+    return userRef.set(data);
   }
 }
